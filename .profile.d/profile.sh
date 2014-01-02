f01ff4492dc03f877656d9a972c566070a60012d
Nate Brown <nbrown.us@gmail.com>
2014-01-02 11:35:11 -0800

    Trying to get back to normal dot files

---
+++ .profile.d/profile.sh
@@ -0,0 +1,9 @@
+export CLICOLOR=1
+export TERM="xterm-color"
+export GREP_OPTIONS='--color=auto -r'
+export PROMPT_COMMAND='__git_ps1 "\n\u@\h \w" " \\\$ "'
+export GIT_PS1_SHOWDIRTYSTATE=true
+export GIT_PS1_SHOWSTASHSTATE=true
+export GIT_PS1_SHOWCOLORHINTS=true
+export GIT_PS1_SHOWUNTRACKEDFILES=true
+export GIT_PS1_SHOWUPSTREAM="auto"
