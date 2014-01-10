1badee28f8ab5436f52d8bac31caadcdd08425c7
Nate Brown <nbrown.us@gmail.com>
2014-01-10 12:11:37 -0800

    Getting crazy with PS1

--- .profile.d/go.sh
+++ .profile.d/go.sh
@@ -1,1 +1,2 @@
 export GOPATH="$HOME"
+export GOROOT="/usr/local/Cellar/go/1.2"
--- .profile.d/profile.sh
+++ .profile.d/profile.sh
@@ -1,7 +1,20 @@
+#!/bin/sh
+
+. $HOME/.profile.d/colors.sh
+
 export CLICOLOR=1
 export TERM="xterm-color"
 export GREP_OPTIONS='--color=auto -r'
-export PROMPT_COMMAND='__git_ps1 "\n\u@\h \w" " \\\$ "'
+
+POINTER_CHAR="⬆"
+PREINFO="\n$POINTER_CHAR $BIBLUE[ "$BICYAN"exit: $BIWHITE$?$BIBLUE ] [ "$BICYAN"time: $BIWHITE\t$BIBLUE ]$END_COLOR"
+PREGIT="$PREINFO\n$BIWHITE\u$BIBLUE@$BIRED\h$BIWHITE \w$END_COLOR"
+POSTGIT=" $BIRED\$$END_COLOR "
+
+#export PROMPT_COMMAND='__git_ps1 "\n\u@\h \w" " \\\$ "'
+#export PROMPT_COMMAND='__git_ps1 "\n[exit: $?] [time: \t]\n\[\e[01;37m\]\u\[\e[01;34m\]@\[\e[01;31m\]\h\[\e[01;37m\] \w\[\e[0m\]" " \[\e[01;31m\]\$\[\e[0m\] "'
+export PROMPT_COMMAND="__git_ps1 \"$PREGIT\" \"$POSTGIT\""
+
 export GIT_PS1_SHOWDIRTYSTATE=true
 export GIT_PS1_SHOWSTASHSTATE=true
 export GIT_PS1_SHOWCOLORHINTS=true
